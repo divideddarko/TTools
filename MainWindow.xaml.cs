6dade3577c84f18b018c79ff7a299c25ab48f02c
Scott Barton <divideddarko@gmail.com>
2020-08-30 23:49:01 +0100

    improve Windows stuff

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace TTools
 {
@@ -25,9 +26,60 @@ namespace TTools
             InitializeComponent();
         }
 
-        void welcomeClick(object sender, RoutedEventArgs e)
+        void moveWindow()
         {
-            MessageBox.Show("This will start the welcome application");
+            this.DragMove();
+        }
+
+        void WindowController(object sender, RoutedEventArgs e)
+        {
+
+            Button values = sender as Button;
+            string senderName = values.Name;
+
+            switch (senderName)
+            {
+                case "minimize":
+                    this.WindowState = WindowState.Minimized;
+                    break;
+                case "resize":
+                    if ((this.WindowState).ToString() != "Maximized")
+                    {
+                        this.WindowState = WindowState.Maximized;
+                    }
+                    else
+                    {
+                        this.WindowState = WindowState.Normal;
+                    }
+                    break;
+                case "close":
+                    System.Windows.Application.Current.Shutdown();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void openMenu(object sender, RoutedEventArgs e)
+        {
+            // Button values = sender as Button;
+
+            // string senderName = values.Name;
+
+            // ImageBrush ib = new ImageBrush();
+            // ib.ImageSource = new BitmapImage(new Uri(@"open-menuopen.png", UriKind.Relative));
+
+            // values.Background = ib;
+
+
+
+
+            MessageBox.Show("Hello");
+        }
+
+        private void OnDragMoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
