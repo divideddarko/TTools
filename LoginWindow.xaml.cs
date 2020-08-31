c6e4c23188d256a71e33e7d4ae7a55984e7c35c7
Scott Barton <divideddarko@gmail.com>
2020-08-31 22:02:54 +0100

    Login Window started

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,36 +16,17 @@ using System.Diagnostics;
 
 namespace TTools
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public static int opacity { get; set; } = 0;
-        public MainWindow()
+        public static void MainLogin()
         {
-            InitializeComponent();
-
-            menuTray.Opacity = opacity;
+            // InitializeComponent();
         }
 
-        void titleBarWindowController(object sender, MouseButtonEventArgs e)
+        private void titleBarWindowController(object sender, RoutedEventArgs e)
         {
-            if (e.ClickCount == 2)
-            {
-                if ((this.WindowState).ToString() != "Maximized")
-                {
-                    this.WindowState = WindowState.Maximized;
-                }
-                else
-                {
-                    this.WindowState = WindowState.Normal;
-                }
-            }
 
-            DragMove();
         }
-
         void WindowController(object sender, RoutedEventArgs e)
         {
             Button values = sender as Button;
@@ -71,24 +52,6 @@ namespace TTools
                     break;
                 default:
                     break;
-            }
-        }
-
-        private void openMenu(object sender, RoutedEventArgs e)
-        {
-            int vis = (menuTray.Opacity == 1) ? 0 : 1;
-            menuTray.Opacity = vis;
-        }
-
-        public void windowSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.BorderThickness = new System.Windows.Thickness(6);
-            }
-            else
-            {
-                this.BorderThickness = new System.Windows.Thickness(0);
             }
         }
     }
